5bcd3ec425aabeaeff2d9165e72865aeddeb505e
Tobias Schwandt <tobias.schwandt@tu-ilmenau.de>
2016-07-07 17:48:14 +0200

    Add spawn point with animation and functionality

---
+++ Assets/Scripts/Obstacles/SpawnPointBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPointBehaviour : MonoBehaviour 
+{
+	public SpawnPointBehaviour m_PreviousSpawnPoint;
+
+	private bool m_IsChecked = false;
+	private bool m_HasPreviousSpawnPoint = false;
+
+	void Start () 
+	{
+		m_HasPreviousSpawnPoint = m_PreviousSpawnPoint != null;
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider _Other)
+	{
+		if (m_IsChecked == false && (m_HasPreviousSpawnPoint == false || m_PreviousSpawnPoint.Checked == true))
+		{
+			GetComponent<Animation>().Play();
+
+			m_IsChecked = true;
+		}
+	}
+
+	public bool Checked
+	{
+		get 
+		{
+			return m_IsChecked;
+		}
+	}
+}
